d65a8cecf96bff68a59d242a39b9f5c379f0ec46
xaota <xaota@users.noreply.github.com>
2018-08-12 12:19:52 +0500

    material-radio

---
+++ components/radio/material-radio.js
@@ -0,0 +1,21 @@
+import Material from '../Material.js'
+const element = 'material-radio';
+
+/** */
+  class MaterialRadio extends Material {
+  /** */
+    constructor() {
+      super(element);
+    }
+
+    // init() {
+    //   const content = this.content;
+    //
+    // }
+  }
+
+customElements.define(element, MaterialRadio);
+
+// #region [Private]
+
+// #endregion

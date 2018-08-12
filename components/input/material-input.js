5348735befe5a518e1401568290104e02a8f59af
xaota <xaota@users.noreply.github.com>
2018-08-12 12:19:43 +0500

    material-input

---
+++ components/input/material-input.js
@@ -0,0 +1,40 @@
+import Material, {$, drawRipple, pointerOffset} from '../Material.js';
+const element = 'material-input';
+
+/**
+ *
+ *
+ * @class MaterialInput
+ * @extends {Material}
+ */
+  class MaterialInput extends Material {
+  /**
+    *
+    */
+    constructor() {
+      super(element, 'closed');
+    }
+
+  /**
+    *
+    */
+    init() {
+      const content = this.content;
+      const root = $('div.root', content);
+      root.addEventListener('click', event => {
+        const position = pointerOffset(root, event);
+        root.style.setProperty('--position', position.x + 'px');
+        drawRipple.call(root, position);
+      });
+      const input = $('div.root > input', content);
+      input.addEventListener('blur', event => {
+        root.style.setProperty('--position', '50%');
+      });
+    }
+  }
+
+customElements.define(element, MaterialInput);
+
+// #region [Private]
+
+// #endregion

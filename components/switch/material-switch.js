9073cd5eabceea35ce13ce468b1958fc0e31769b
xaota <xaota@users.noreply.github.com>
2018-08-12 12:20:06 +0500

    material-switch

---
+++ components/switch/material-switch.js
@@ -0,0 +1,38 @@
+import Material, {$, drawRipple, pointerOffset} from '../Material.js';
+const element = 'material-switch';
+
+/** */
+  class MaterialSwitch extends Material {
+  /** */
+    constructor() {
+      super(element, 'closed');
+    }
+
+  /** */
+    init() {
+      const content = this.content;
+
+      return this;
+    }
+  }
+
+customElements.define(element, MaterialSwitch);
+
+// #region [Private]
+
+// #endregion
+
+/** */
+  function init() {
+    const content = this.content;
+    const root = $('div.root', content);
+    root.addEventListener('click', event => {
+      const position = pointerOffset(root, event);
+      root.style.setProperty('--position', position.x + 'px');
+      drawRipple.call(root, position);
+    });
+    const input = $('div.root > input', content);
+    input.addEventListener('blur', event => {
+      root.style.setProperty('--position', '50%');
+    });
+  }

196065fb51a83379aec25c60d07288b73004e0c3
xaota <xaota@users.noreply.github.com>
2018-08-12 12:19:20 +0500

    material-chip

---
+++ components/chip/material-chip.js
@@ -0,0 +1,21 @@
+import Material from '../Material.js'
+const element = 'material-chip';
+
+/** */
+  class MaterialChip extends Material {
+    /** */
+      constructor() {
+        super(element);
+      }
+
+      // init() {
+      //   const content = this.content;
+
+      // }
+  }
+
+customElements.define(element, MaterialChip);
+
+// #region [Private]
+
+// #endregion

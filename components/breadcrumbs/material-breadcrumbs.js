025c21496eee0f2314723ab3535eff222ddc809c
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2018-10-13 14:43:07 +0500

    #20 | BreadCrumbs | Путь по карте сайта

---
+++ components/breadcrumbs/material-breadcrumbs.js
@@ -0,0 +1,16 @@
+import Material from '../../script/Material.js'
+
+const component = Material.meta(import.meta.url, 'material-breadcrumbs');
+/**
+  *
+  */
+  export default class MaterialBreadcrumbs extends Material {
+  /**
+    *
+    */
+    constructor() {
+      super(component);
+    }
+  }
+
+Material.define(component, MaterialBreadcrumbs);

5b20e3f9464c31eccbed2c8e0b5785872e75d743
Поташин Андрей <potashin@ro.ru>
2018-10-14 17:41:10 +0500

    Отображение аватарок

--- components/avatar/material-avatar.js
+++ components/avatar/material-avatar.js
@@ -11,6 +11,37 @@ const component = Material.meta(import.meta.url, 'material-avatar');
     constructor() {
       super(component);
     }
+
+    mount(content) {
+      const src = this.src;
+      update(content, src);
+    }
+
+    static get observedAttributes() {
+      return ['src'];
+    }
+
+    attributeChangedCallback(name, previous, current) {
+      update(this.shadowRoot, current);
+    }
+
+    /** */
+      get src() {
+        return this.getAttribute("src");
+      }
+
+    /** */
+      set src(value) {
+        !!value
+          ? this.setAttribute("src", value)
+          : this.removeAttribute("src");
+      }
   }
 
 Material.define(component, MaterialAvatar);
+
+function update(root, src) {
+  const image = root.querySelector('img');
+  if (!image) return;
+  image.src = src;
+}

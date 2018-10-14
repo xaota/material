74956311d031ceb480a8810266e5ef4ea6496a92
Поташин Андрей <potashin@ro.ru>
2018-10-15 02:05:40 +0500

    Установка размеров аватара

--- components/avatar/material-avatar.js
+++ components/avatar/material-avatar.js
@@ -13,16 +13,16 @@ const component = Material.meta(import.meta.url, 'material-avatar');
     }
 
     mount(content) {
-      const src = this.src;
-      update(content, src);
+      update(content, this.src);
     }
 
     static get observedAttributes() {
-      return ['src'];
+      return ['src', 'size'];
     }
 
     attributeChangedCallback(name, previous, current) {
-      update(this.shadowRoot, current);
+      if (name === 'src')  update(this.shadowRoot, current);
+      if (name === 'size') resize(this, current);
     }
 
     /** */
@@ -36,12 +36,30 @@ const component = Material.meta(import.meta.url, 'material-avatar');
           ? this.setAttribute("src", value)
           : this.removeAttribute("src");
       }
+
+    /** */
+      get size() {
+        return this.getAttribute("size");
+      }
+
+    /** */
+      set size(value) {
+        !!value
+          ? this.setAttribute("size", value)
+          : this.removeAttribute("size");
+      }
   }
 
 Material.define(component, MaterialAvatar);
 
-function update(root, src) {
-  const image = root.querySelector('img');
-  if (!image) return;
-  image.src = src;
-}
+/** */
+  function update(root, src) {
+    const image = root.querySelector('img');
+    if (!image) return;
+    image.src = src;
+  }
+
+/** */
+  function resize(root, size) {
+    root.style.setProperty('--size', size);
+  }

e16a46aa2a90e008c927ff2f25e1b4d754fade2d
Поташин Андрей <potashin@ro.ru>
2018-10-14 13:01:16 +0500

    Иконки

--- components/icon/material-icon.js
+++ components/icon/material-icon.js
@@ -11,6 +11,19 @@ const component = Material.meta(import.meta.url, 'material-icon');
     constructor() {
       super(component);
     }
+
+  /** */
+    mount(content) {
+      this;
+      const slot = content.querySelector('slot');
+      const root = content.querySelector('img');
+      const theme = 'light';
+      slot.addEventListener('slotchange', () => {
+        const icon = slot.assignedNodes()[0].nodeValue.trim();
+        root.setAttribute('alt', icon);
+        root.src = new URL(`../../icons/${theme}/${icon}.svg`, component.base);
+      });
+    }
   }
 
 Material.define(component, MaterialIcon);

ac2783738a0c88adeb1d5aac22884c4b2688e79d
Ринат Ибрагимов <xao@xaota-macbook-pro.local>
2019-10-26 19:52:20 +0500

    Добавлена возможность корректного отображения списка под сообщениями

--- components/message/material-message.js
+++ components/message/material-message.js
@@ -1,4 +1,4 @@
-import Material from '../../script/Material.js'
+import Material from '../../script/Material.js';
 
 const component = Material.meta(import.meta.url, 'material-message');
 /**
@@ -12,6 +12,21 @@ const component = Material.meta(import.meta.url, 'material-message');
       super(component);
     }
 
+  /** Создание элемента в DOM (DOM доступен) / mount @lifecycle
+    * @param {HTMLElement} node ShadowRoot узел элемента
+    * @return {MaterialCheckbox} @this
+    */
+    mount(node) {
+      this;
+      // super.mount(node, updateAttribute);
+      const slot   = node.querySelector('slot:not([name])');
+      const markup = node.querySelector('slot[name="markup"]');
+
+      resizeMarkup(slot, markup);
+      slot  .addEventListener('slotchange', _ => resizeMarkup(slot, markup));
+      markup.addEventListener('slotchange', _ => resizeMarkup(slot, markup));
+    }
+
   /** Является ли узел элементом {MaterialMessage} @static
     * @param {HTMLElement} node проверяемый узел
     * @return {boolean} node instanceof MaterialMessage
@@ -22,3 +37,16 @@ const component = Material.meta(import.meta.url, 'material-message');
   }
 
 Material.define(component, MaterialMessage);
+
+// #region [Private]
+/** */
+  function resizeMarkup(slot, markup) {
+    // console.log(1, slot);
+    setTimeout(() => {
+      // const width = getComputedStyle(slot); // .width;
+      const width = slot.getBoundingClientRect().width;
+      // console.log(width);
+      markup.style.width = width + 'px';
+    }, 10);
+  }
+// #endregion
